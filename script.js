27921fc53fa35f274ab409a976c10c9e56a78bdc
Zurishaddai Temitope <temitopezurishaddai@gmail.com>
2022-10-14 04:51:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.getElementById('menu-bar')
+let navbar = document.getElementsByClassName('navbar')
+
+menu.onclick = () => {
+
+    
+    
+    navbar.classList.toggle('hide')
+}
+
